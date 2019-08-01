2e39dde1975b4d797727adf4fa8f30ad0c12719f
Christian Ramelow <info@codekandis.net>
2019-08-01 13:41:37 +0200

    Fixes the options request.

--- src/Curl/OptionsRequest.php
+++ src/Curl/OptionsRequest.php
@@ -39,10 +39,9 @@ class OptionsRequest implements OptionsRequestInterface
 			[
 				CURLOPT_URL            => $this->url,
 				CURLOPT_CUSTOMREQUEST  => Methods::OPTIONS,
-				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_HEADER         => true,
-				CURLOPT_NOBODY         => true,
-				CURLOPT_VERBOSE        => true,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_NOBODY         => true
 			]
 		);
 		$response = curl_exec( $curlHandler );
