d1e0e5bdc1f5232e155a6d32d03d17db5e8ca8a8
Christian Ramelow <info@codekandis.net>
2019-08-01 11:44:08 +0200

    Removes the obsolete constructor of the routes configuration.

--- src/Http/RoutesConfiguration.php
+++ src/Http/RoutesConfiguration.php
@@ -5,11 +5,6 @@ use CodeKandis\Tiphy\Configurations\AbstractConfiguration;
 
 class RoutesConfiguration extends AbstractConfiguration implements RoutesConfigurationInterface
 {
-	public function __construct( string $path )
-	{
-		parent::__construct( $path );
-	}
-
 	public function getRoutes(): array
 	{
 		return $this->data;
