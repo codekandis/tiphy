94475812c2bebc69d542beb8f6bf28d7322754ba
Christian Ramelow <info@codekandis.net>
2019-08-14 12:00:52 +0200

    Removes the obsolete `StatusCodeMessageInterpreterInterface`.

--- src/Http/Responses/StatusCodeMessageInterpreterInterface.php
+++
@@ -1,7 +0,0 @@
-<?php declare( strict_types = 1 );
-namespace CodeKandis\Tiphy\Http\Responses;
-
-interface StatusCodeMessageInterpreterInterface
-{
-	public function interpret( int $statusCode ): string;
-}
