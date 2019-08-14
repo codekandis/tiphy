1ec7aba12287b4ff00e43f8f4f5d7298edf03574
Christian Ramelow <info@codekandis.net>
2019-08-14 23:12:49 +0200

    Adds a missing exception hint in the `InternalServerErrorAction`.

--- src/Actions/InternalServerErrorAction.php
+++ src/Actions/InternalServerErrorAction.php
@@ -5,9 +5,13 @@ use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
 use CodeKandis\Tiphy\Http\Responses\StatusMessages;
 use CodeKandis\Tiphy\Throwables\ErrorInformation;
+use JsonException;
 
 class InternalServerErrorAction implements ActionInterface
 {
+	/**
+	 * @throws JsonException
+	 */
 	public function execute(): void
 	{
 		$errorInformation = new ErrorInformation( StatusCodes::INTERNAL_SERVER_ERROR, StatusMessages::INTERNAL_SERVER_ERROR );
