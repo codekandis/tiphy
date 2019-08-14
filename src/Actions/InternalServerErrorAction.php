521a4bd095236aa3112768024a7e24e317a92c04
Christian Ramelow <info@codekandis.net>
2019-08-14 22:19:48 +0200

    Adds the `InternalServerErrorAction`.

---
+++ src/Actions/InternalServerErrorAction.php
@@ -0,0 +1,17 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Actions;
+
+use CodeKandis\Tiphy\Http\Responses\JsonResponder;
+use CodeKandis\Tiphy\Http\Responses\StatusCodes;
+use CodeKandis\Tiphy\Http\Responses\StatusMessages;
+use CodeKandis\Tiphy\Throwables\ErrorInformation;
+
+class InternalServerErrorAction implements ActionInterface
+{
+	public function execute(): void
+	{
+		$errorInformation = new ErrorInformation( StatusCodes::INTERNAL_SERVER_ERROR, StatusMessages::INTERNAL_SERVER_ERROR );
+		( new JsonResponder( StatusCodes::INTERNAL_SERVER_ERROR, null, $errorInformation ) )
+			->respond();
+	}
+}
