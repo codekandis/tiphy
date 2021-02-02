48882bdc585174bc3dac24a6c98415fb392b1333
Christian Ramelow <info@codekandis.net>
2021-02-02 05:46:13 +0100

    Updates the class `NotFoundAction`.

--- src/Actions/NotFoundAction.php
+++ src/Actions/NotFoundAction.php
@@ -1,6 +1,7 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Tiphy\Actions;
 
+use CodeKandis\Tiphy\Entities\NotFoundEntity;
 use CodeKandis\Tiphy\Http\Responses\JsonResponder;
 use CodeKandis\Tiphy\Http\Responses\StatusCodes;
 use CodeKandis\Tiphy\Http\Responses\StatusMessages;
@@ -15,12 +16,27 @@ use JsonException;
 class NotFoundAction implements ActionInterface
 {
 	/**
+	 * Stores the not found entity describing the request.
+	 * @var NotFoundEntity
+	 */
+	private NotFoundEntity $notFoundEntity;
+
+	/**
+	 * Constructor method.
+	 * @param NotFoundEntity $notFoundEntity The not found entity representing the request.
+	 */
+	public function __construct( NotFoundEntity $notFoundEntity )
+	{
+		$this->notFoundEntity = $notFoundEntity;
+	}
+
+	/**
 	 * @inheritDoc
 	 * @throws JsonException An error occurred during the creation of the JSON response.
 	 */
 	public function execute(): void
 	{
-		$errorInformation = new ErrorInformation( StatusCodes::NOT_FOUND, StatusMessages::NOT_FOUND );
+		$errorInformation = new ErrorInformation( StatusCodes::NOT_FOUND, StatusMessages::NOT_FOUND, $this->notFoundEntity );
 		( new JsonResponder( StatusCodes::NOT_FOUND, null, $errorInformation ) )
 			->respond();
 	}
