e7eb1eb7428188e64f4c6e9dfe8d6083f91fe8c2
Christian Ramelow <info@codekandis.net>
2021-02-23 16:12:57 +0100

    Adds the interface `CommandInterface`.

---
+++ src/Commands/CommandInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Commands;
+
+/**
+ * Represents the interface of all commands.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface CommandInterface
+{
+}
