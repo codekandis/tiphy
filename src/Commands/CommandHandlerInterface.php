acf606601df6c50ad0280f331f40592ab6f475b8
Christian Ramelow <info@codekandis.net>
2021-02-23 14:17:53 +0100

    Adds the interface `CommandHandlerInterface`.

---
+++ src/Commands/CommandHandlerInterface.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Commands;
+
+/**
+ * Represents the interface of all command handlers.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface CommandHandlerInterface
+{
+	/**
+	 * Executes the command handler.
+	 * @return CommandHandlerResultInterface The result of the command handler execution.
+	 */
+	public function execute(): CommandHandlerResultInterface;
+}
