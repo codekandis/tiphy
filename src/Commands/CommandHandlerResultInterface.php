a8aa5821e83c955510d205f10e51bef840c17ba8
Christian Ramelow <info@codekandis.net>
2021-02-23 14:17:04 +0100

    Adds the interface `CommandHandlerResultInterface`.

---
+++ src/Commands/CommandHandlerResultInterface.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Commands;
+
+use CodeKandis\Tiphy\Throwables\ErrorInformationInterface;
+
+/**
+ * Represents the interface of all command handler results.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface CommandHandlerResultInterface
+{
+	/**
+	 * Gets the state of the command handler result.
+	 * @return bool True if the command handler has been executed successfully, false otherwise.
+	 */
+	public function getState(): bool;
+
+	/**
+	 * Gets the errors if the command handler failed to execute.
+	 * @return ErrorInformationInterface[] The errors if the command handler failed to execute.
+	 */
+	public function getErrors(): array;
+}
