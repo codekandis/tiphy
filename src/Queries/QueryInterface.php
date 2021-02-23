819da17f65302516c94292535deabe17379b5139
Christian Ramelow <info@codekandis.net>
2021-02-23 17:36:24 +0100

    Adds the interface `QueryInterface`.

---
+++ src/Queries/QueryInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Queries;
+
+/**
+ * Represents the interface of all queries.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface QueryInterface
+{
+}
