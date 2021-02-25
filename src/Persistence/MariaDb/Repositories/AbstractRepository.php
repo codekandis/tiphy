50807a8b9423f46326473274219c08de67ca0d37
Christian Ramelow <info@codekandis.net>
2021-02-25 12:03:57 +0100

    Updates the class `AbstractRepository`.

--- src/Persistence/MariaDb/Repositories/AbstractRepository.php
+++ src/Persistence/MariaDb/Repositories/AbstractRepository.php
@@ -1,14 +1,16 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Tiphy\Persistence\MariaDb\Repositories;
 
+use Closure;
 use CodeKandis\Tiphy\Persistence\MariaDb\ConnectorInterface;
+use CodeKandis\Tiphy\Persistence\MariaDb\StatementExecutionFailedException;
 
 /**
  * Represents the base class of all MariaDb repositories.
  * @package codekandis/tiphy
  * @author Christian Ramelow <info@codekandis.net>
  */
-abstract class AbstractRepository
+abstract class AbstractRepository implements RepositoryInterface
 {
 	/**
 	 * Stores the MariaDb database connector.
@@ -23,5 +25,25 @@ abstract class AbstractRepository
 	public function __construct( ConnectorInterface $connector )
 	{
 		$this->databaseConnector = $connector;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function asTransaction( Closure $closure, array...$arguments )
+	{
+		try
+		{
+			$this->databaseConnector->beginTransaction();
+			$returnValue = $closure( ...$arguments );
+			$this->databaseConnector->commit();
+		}
+		catch ( StatementExecutionFailedException $exception )
+		{
+			$this->databaseConnector->rollback();
+			throw $exception;
+		}
+
+		return $returnValue;
 	}
 }
