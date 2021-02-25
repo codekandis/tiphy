3b5a87808fb5bca73a29e5e8bb0b44230407de2e
Christian Ramelow <info@codekandis.net>
2021-02-25 04:50:49 +0100

    Adds the exception `StatementPreparationFailedExeception`.

---
+++ src/Persistence/MariaDb/StatementPreparationFailedException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Persistence\MariaDb;
+
+use CodeKandis\Tiphy\Persistence\PersistenceException;
+
+/**
+ * Represents an exception if the preparation of a statement failed.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class StatementPreparationFailedException extends PersistenceException
+{
+}
