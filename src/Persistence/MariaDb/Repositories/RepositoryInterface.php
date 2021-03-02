37460653423fa252e531af4717d3a83419344249
Christian Ramelow <info@codekandis.net>
2021-03-02 12:43:57 +0100

    Updates the interface `RepositoryInterface`.

--- src/Persistence/MariaDb/Repositories/RepositoryInterface.php
+++ src/Persistence/MariaDb/Repositories/RepositoryInterface.php
@@ -2,6 +2,9 @@
 namespace CodeKandis\Tiphy\Persistence\MariaDb\Repositories;
 
 use Closure;
+use CodeKandis\Tiphy\Persistence\MariaDb\TransactionCommitFailedException;
+use CodeKandis\Tiphy\Persistence\MariaDb\TransactionRollbackFailedException;
+use CodeKandis\Tiphy\Persistence\MariaDb\TransactionStartFailedException;
 
 /**
  * Represents the interface of all MariaDB repositories.
@@ -15,6 +18,9 @@ interface RepositoryInterface
 	 * @param Closure $closure The closure to invoke.
 	 * @param array $closureArguments The arguments to pass to the closure.
 	 * @return mixed The return value of the closure.
+	 * @throws TransactionStartFailedException The transaction failed to start.
+	 * @throws TransactionRollbackFailedException The transaction failed to roll back.
+	 * @throws TransactionCommitFailedException The transaction failed to commit.
 	 */
 	public function asTransaction( Closure $closure, array ...$closureArguments );
 }
