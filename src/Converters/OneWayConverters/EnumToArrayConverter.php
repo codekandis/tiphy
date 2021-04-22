c7c8920cc735b2f34fb0f3821d56408ed8df0c3b
Christian Ramelow <info@codekandis.net>
2021-04-22 16:45:34 +0200

    Adds the class `EnumToArrayConverter`.

---
+++ src/Converters/OneWayConverters/EnumToArrayConverter.php
@@ -0,0 +1,54 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Converters\OneWayConverters;
+
+use CodeKandis\Tiphy\Converters\OneWayConverterInterface;
+use ReflectionClass;
+use ReflectionException;
+use function in_array;
+use function sprintf;
+
+/**
+ * Represents an converter converting enums to arrays of values.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class EnumToArrayConverter implements OneWayConverterInterface
+{
+	/**
+	 * Represents the error message if an enum class does not exist.
+	 * @var string
+	 */
+	public const ERROR_ENUM_CLASS_DOES_NOT_EXIST = 'The enum class `%s` does not exist.';
+
+	/**
+	 * {@inheritDoc}
+	 * @throws EnumClassNotFoundException The enum class does not exist.
+	 */
+	public function convert( $value )
+	{
+		try
+		{
+			$reflectedClass = new ReflectionClass( $value );
+		}
+		catch ( ReflectionException $exception )
+		{
+			throw new EnumClassNotFoundException(
+				sprintf(
+					static::ERROR_ENUM_CLASS_DOES_NOT_EXIST,
+					$value
+				)
+			);
+		}
+
+		$convertedValue = [];
+		foreach ( $reflectedClass->getConstants() as $constantValue )
+		{
+			if ( false === in_array( $constantValue, $convertedValue, true ) )
+			{
+				$convertedValue[] = $constantValue;
+			}
+		}
+
+		return $convertedValue;
+	}
+}
