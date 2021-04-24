1f4c2bb15da98f7ce71bd4e1e72d47239cb59735
Christian Ramelow <info@codekandis.net>
2021-04-24 11:27:05 +0200

    Adds the exception `ArrayKeyNotFoundException`.

---
+++ src/Validators/ArrayValidators/ArrayKeyNotFoundException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Validators\ArrayValidators;
+
+use RuntimeException;
+
+/**
+ * Represents an exception if an array key does not exist.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ArrayKeyNotFoundException extends RuntimeException
+{
+}
