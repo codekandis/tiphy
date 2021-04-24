d6379b22a0ec5a6706ef7cc182748465e8c54829
Christian Ramelow <info@codekandis.net>
2021-04-24 11:25:15 +0200

    Updates the interface `ArrayValidatorMappingsInterface`.

--- src/Validators/ArrayValidators/ArrayValidatorMappingsInterface.php
+++ src/Validators/ArrayValidators/ArrayValidatorMappingsInterface.php
@@ -2,19 +2,48 @@
 namespace CodeKandis\Tiphy\Validators\ArrayValidators;
 
 use Countable;
+use Iterator;
 
 /**
  * Represents the interface of all array validator mapping lists.
  * @package codekandis/tiphy
  * @author Christian Ramelow <info@codekandis.net>
  */
-interface ArrayValidatorMappingsInterface extends Countable
+interface ArrayValidatorMappingsInterface extends Countable, Iterator
 {
 	/**
 	 * Gets the amount of array validator mappings in the list.
 	 * @return int The amount of array validator mappings in the list.
 	 */
 	public function count(): int;
+
+	/**
+	 * Gets the current array validator mapping of the list.
+	 * @return ArrayValidatorMappingInterface The current array validator mapping of the list.
+	 */
+	public function current();
+
+	/**
+	 * Gets the key of the current array validator mapping of the list.
+	 * @return scalar The key of the current array validator mapping of the list.
+	 */
+	public function key();
+
+	/**
+	 * Moves the internal pointer to the next array validator mapping of the list.
+	 */
+	public function next(): void;
+
+	/**
+	 * Rewinds the internal pointer to the first array validator mapping of the list.
+	 */
+	public function rewind(): void;
+
+	/**
+	 * Determines if the internal pointer points to an array validator mapping of the list.
+	 * @return bool True if the internal pointer points to an array validator mapping of the list, false otherwise.
+	 */
+	public function valid(): bool;
 
 	/**
 	 * Adds array validator mappings to the list.
