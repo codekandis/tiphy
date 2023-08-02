fad7a080abcaaf42240e24c5e4562c0cea987778
Christian Ramelow <info@codekandis.net>
2023-08-02 21:59:51 +0200

    Adds an abstract URI builder builder.

---
+++ src/Http/UriBuilders/UriBuilderBuilderInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Tiphy\Http\UriBuilders;
+
+/**
+ * Represents the interface of any URI builder builder.
+ * @package codekandis/tiphy
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface UriBuilderBuilderInterface
+{
+}
